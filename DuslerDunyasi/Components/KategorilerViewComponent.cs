a0fc8d9515f94228bcffe04311a51ca954fbbdac
Mustafa <mustafa.asill@hotmail.com>
2023-01-04 17:45:14 +0300

    filitrele

---
+++ DuslerDunyasi/Components/KategorilerViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DuslerDunyasi.Components
+{
+    public class KategorilerViewComponent :ViewComponent
+    {
+        private readonly UygulamaDbContext _db;
+
+        public KategorilerViewComponent(UygulamaDbContext db)
+        {
+            _db = db;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(_db.Kategoriler.ToList());
+        }
+    }
+}
