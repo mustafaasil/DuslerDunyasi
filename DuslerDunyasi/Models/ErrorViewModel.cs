a1240c8f62b11742784c0af2f703b619cb31c200
Mustafa <mustafa.asill@hotmail.com>
2023-01-04 14:09:20 +0300

    Add project files.

---
+++ DuslerDunyasi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DuslerDunyasi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
