921eae04a378fef8bc132815a550e17e4ac74382
Mustafa <mustafa.asill@hotmail.com>
2023-01-06 17:38:14 +0300

    Gönderiler

---
+++ DuslerDunyasi/Areas/Admin/Controllers/GonderilerController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace DuslerDunyasi.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class GonderilerController : Controller
+    {
+        private readonly UygulamaDbContext _db;
+
+        public GonderilerController(UygulamaDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View(_db.Gonderiler.Include(x=>x.Kategori).ToList()); //Inculude ile ilişkili oldugu kategorileri getir
+        }
+    }
+}
--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 .Net 6 ile geliþtirilen bir MVC Blog projesi. Arayüzünde Bootstrap 5 kullanýldý
 
 ## Kaynaklar
-*https://getbootstrap.com/docs/5.3/examples/blog/*
-*https://startbootstrap.com/template/sb-admin*
+*https://getbootstrap.com/docs/5.3/examples/blog/
+*https://startbootstrap.com/template/sb-admin
+*https://datatables.net/
+*https://datatables.net/examples/styling/bootstrap5.html*
